850e6a36ca3727750f47f9a3550c8fff18f1b76f
azaharul-just <you@example.com>
2019-12-21 12:57:23 +0600

    index added

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Student/Employee Management System</title>
+  </head>
+  <body>
+    <h1>It's plateform ok!</h1>
+  </body>
+</html>
