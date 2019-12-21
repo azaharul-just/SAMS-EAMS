7285bdc1ed13c426f2a81cda278d6197ec77bb04
azaharul-just <you@example.com>
2019-12-21 13:26:26 +0600

    header and footer added in index file

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Student/Employee Management System</title>
-  </head>
-  <body>
-    <h1>It's plateform ok!</h1>
-  </body>
-</html>
+<?php include('inc/header.php'); ?>
+    
+<?php include('inc/footer.php'); ?>
