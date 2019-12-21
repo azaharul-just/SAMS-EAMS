dc9496ce7ee90853fed4a2ba577a5e6ce6793658
azaharul-just <you@example.com>
2019-12-21 13:08:59 +0600

    inc folder added which is include header,footer and others nedded files

---
+++ inc/footer.php
@@ -0,0 +1,22 @@
+ 
+		 <div class="card text-center bg-info">
+		 	<div class="card-header">
+			 	<div class="row">
+					<div class="col-md-6">
+						<p>Email : azaharul.csejust@gmail.com</p>
+					</div>
+					<div class="col-md-6">
+					<p> 
+					<span class=""> www.facebook.com/ahappyboy </span>
+					</p>
+					</div>
+				
+				</div>
+		 	</div>
+		 </div>
+		 <br>
+		
+	</div>
+ 
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SAMS/EAMS</title>
+	<link rel="stylesheet" type="text/css" href="inc/bootstrap.min.css">
+	<script type="text/javascript" src="inc/jquery.min.js"></script>
+	<script type="text/javascript" src="inc/bootstrap.min.js"></script>
+</head>
+<body style="background: gray">
+	<div class="container"> 
+		 <div class="card bg-info">
+		 	<div class="card-header text-center" style="height: 120px;">
+			 	<div class="row">
+					<div class="col-md-12"> 
+						<br>
+						<h1>Student/Employee Attandance Management System</h1>
+					</div>
+				
+				</div>
+		 	</div>
+		 </div>
+		 
