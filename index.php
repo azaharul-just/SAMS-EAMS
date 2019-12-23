97ad40bc9cc9551c2ca813989aaf33ca5e0d9ae5
azaharul-just <you@example.com>
2019-12-23 10:22:19 +0600

    index file updated

--- index.php
+++ index.php
@@ -1,49 +1,108 @@
-<?php include('inc/header.php'); ?>
+<?php
+ include('inc/header.php'); 
+ include('lib/Student.php');
+?>
 
-<div class="card"  style="min-height: 500px">
-    <div class="card-header">
-      <a href="add.php" class="btn btn-success">Add Student</a>
-      <a href="date_view.php" class="btn btn-info float-right">View All</a>
-    </div>
-    <div class="card-body">
-      <div>
-        <h3 style="float: right;color: gray">Date:<?ph ehco date();?></h3>
-      </div>
-      <div>
-        <h2 style=" color: gray"><u> Attendance Form : </u></h2>
-      </div>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $("form").submit(function(){
+      var roll = true;
+      $(':radio').each(function(){
+        name = $(this).attr('name');
+        if(roll && !$(':radio[name="' + name + '"]:checked').length) {
+          alert(name + " Roll mising!");
+          //$('.alert').show();
+          roll = false;
+        }
+      });
+      return roll;
+    });
+  });
 
-    <div class='alert alert-danger' style="display: none;">Error! Student Attandence Missing!</div>
+</script>
 
-      <form action="" method="post">
+ 
 
-        <table class="table table-striped text-center">
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>ID</th>
-            <th>Action</th>
-          </tr>
+
+<?php 
+  error_reporting(0);
+  $stu = new Student;
+  $cur_date = date('Y-m-d');
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $attend = $_POST['attend'];
+
+    $insertAttendance = $stu->insertAttendance($cur_date,$attend);
+
+  }
+?>
+      <div class="card"  style="min-height: 500px">
+        <div class="card-header">
+          <a href="add.php" class="btn btn-success">Add Student</a> 
+          <a href="date_view.php" class="btn btn-info float-right">View All</a>
+        </div>
+        <div class="card-body">
+          <div>
+            <h3 style="float: right;color: gray">Date: <?php echo $cur_date;?></h3>
+          </div>
+          <div>
+            <h2 style=" color: gray"><u> Attendance Form : </u></h2>
+          </div>
+
+        <?php 
+          if (isset($insertAttendance)) {
+            echo $insertAttendance;
+          }
+
+        ?>
+
+        <div class='alert alert-danger' style="display: none;">Error! Student Attandence Missing!</div>
+
+          <form action="" method="post">
+
+            <table class="table table-striped text-center">
               <tr>
-                <td> </td>
-                <td> </td>
-                <td> </td>
-                <td>
-                  <input type="radio" name="attend" value="present">P
-                  <input type="radio" name="attend" value="absent">A
+                <th>#</th>
+                <th>Name</th>
+                <th>ID</th>
+                <th>Action</th>
+              </tr>
+
+            <?php 
+              $get_student = $stu->getStudents();
+              if ($get_student) {
+                $i=0;
+                while ($data = $get_student->fetch_array()) { 
+                  $i++;
+            ?>
+
+                  <tr>
+                    <td><?php echo $i;?></td>
+                    <td><?php echo $data['name'];?></td>
+                    <td><?php echo $data['roll'];?></td>
+                    <td>
+                      <input type="radio" name="attend[<?php echo $data['roll'];?>]" value="present">P
+                      <input type="radio" name="attend[<?php echo $data['roll'];?>]" value="absent">A
+                    </td>
+                  </tr>
+
+
+            <?php } }  ?>
+             
+
+              
+               
+              <tr>
+                <td colspan="4">
+                  <input type="submit" name="submit" class="btn btn-success" value="Add Attendance">
                 </td>
               </tr>
-          <tr>
-            <td colspan="4">
-              <input type="submit" name="submit" class="btn btn-success" value="Add Attendance">
-            </td>
-          </tr>
+               
+            </table>
+            
+          </form>
+          
+        </div>
+         
+      </div>
 
-        </table>
-
-      </form>
-
-    </div>
-
-  </div>
 <?php include('inc/footer.php'); ?>
